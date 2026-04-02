64c4b7a09cca25e011c585911e88d8fad7c4789b
gbbpro <78455428+gbbpro@users.noreply.github.com>
2026-04-01 20:21:46 -0600

    fixed debug error

--- app/api/debug/route.ts
+++ app/api/debug/route.ts
@@ -3,7 +3,7 @@ import { cacheGet } from "@/lib/cache";
 
 export const dynamic = "force-dynamic";
 
-export default async function GET() {
+export async function GET() {
   const feeds = await cacheGet("user_feeds");
   const version = await cacheGet("user_feeds_version");
 
